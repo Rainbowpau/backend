391586be752d6f78e5155a4be27ba78d19c96798
Rainbowpau <102171986+Rainbowpau@users.noreply.github.com>
2022-10-23 00:51:41 -0300

    cambios

--- src/app.js
+++ src/app.js
@@ -4,6 +4,22 @@ const app = express ()
 const PORT = 3000
 app.listen(PORT, ()=> console.log('escuchando en el puerto', PORT))
 
-app.get('/home', (req , res) => {
+app.get('/index', (req , res) => {
   res.send('estoy en el home')
+} )
+
+app.get('/carrito', (req , res) => {
+  res.send('estoy en el carrito')
+} )
+
+app.get('/login', (req , res) => {
+  res.send('estoy en el login')
+} )
+
+app.get('/product-detail', (req , res) => {
+  res.send('estoy en producto')
+} )
+
+app.get('/registro', (req , res) => {
+  res.send('estoy en el registro')
 } )
