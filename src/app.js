8cf320b166cae2429f7521c2fcb9811730e70ee3
Rainbowpau <102171986+Rainbowpau@users.noreply.github.com>
2022-11-02 17:29:22 -0300

    cambios de segunda entrega

--- src/app.js
+++ src/app.js
@@ -1,25 +1,31 @@
-const express = require ('express')
-const app = express ()
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 const PORT = 3000
-app.listen(PORT, ()=> console.log('escuchando en el puerto', PORT))
+app.listen(PORT, () => console.log('escuchando en el puerto', PORT))
 
-app.get('/index', (req , res) => {
-  res.send('estoy en el home')
-} )
+app.get('/', (req, res) => {
+  // res.send('estoy en el home')
+  res.render('index');
+})
 
-app.get('/carrito', (req , res) => {
-  res.send('estoy en el carrito')
-} )
+app.get('/carrito', (req, res) => {
+  res.render('carrito')
+})
 
-app.get('/login', (req , res) => {
-  res.send('estoy en el login')
-} )
+app.get('/login', (req, res) => {
+  res.render('login')
+})
 
-app.get('/product-detail', (req , res) => {
-  res.send('estoy en producto')
-} )
+app.get('/product-detail', (req, res) => {
+  res.render('product-detail')
+})
 
-app.get('/registro', (req , res) => {
-  res.send('estoy en el registro')
-} )
+app.get('/registro', (req, res) => {
+  res.render('registro')
+})
